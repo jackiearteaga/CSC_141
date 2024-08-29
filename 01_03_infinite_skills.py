e8401c92e1595cf4454bd9cf183bdf87913ae8ed
Jackie Arteaga <guadalupe.arteaga001@albright.edu>
2024-08-29 09:04:11 -0400

    Add files via upload

---
+++ 01_03_infinite_skills.py
@@ -0,0 +1,14 @@
+# Infinite Skills
+# Reflect on your previous programming experiences and how they might help you in this course
+
+# I took a beginner CS course in highschool and I believe it helped me understand basic concepts like syntax, structure, debugging my own code, and using my problem solving skills.
+
+# If I had infinite programming skills, I'd create a personalized AI Health assistant that hospitals and people around the world can use.
+
+# Goal for this course: Go from beginner to at least close to intermediate level in programming.
+
+# It would be awesome to create a program for my aunt and uncle's garbage disposal company to use for when the drivers need a specialized route created for them to make the job easier and safer.
+
+# Creating a super secured smart home management system could be incredibly efficient and integrate eith various smart devices and systems in a home.
+
+# Creating a program for the better of education sounds really intruiging. A personalized learning platform could be tailored to each specific student based on learning styles, weaknesses, strengths, and progress speed.
