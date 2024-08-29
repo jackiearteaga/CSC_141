7ecf3e0295e6a208a86a252c3db1d05cb26782ea
Jackie Arteaga <guadalupe.arteaga001@albright.edu>
2024-08-29 08:35:30 -0400

    Add files via upload

--- 01_01_python_org.py
+++ 01_01_python_org.py
@@ -1,1 +1,11 @@
- # I want to be 
+ # Python has many valuable resources you can use to code away on all of your projects and assignment.
+
+# They provide multiple download option for different operating systems.
+
+# The Python website has various modules and tutorials for both beginners and advanced users.
+
+# There is a "News" section on the website that provides updates on releases and major announcments.
+
+# Python's helpful resources include articles, success stories, and user guides. There are also links to third-party tutorials and tools.
+
+# There is an about section that offers insights into the Python Software Foundation, its mission, and how to become a member.
