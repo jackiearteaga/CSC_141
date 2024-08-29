51cc60ee5b5d55fbfd5139a4ac25fe359787f68e
Jackie Arteaga <guadalupe.arteaga001@albright.edu>
2024-08-29 08:46:59 -0400

    Add files via upload

---
+++ 01_02_hello_world_typos.py
@@ -0,0 +1,8 @@
+# There will be typos. Create a simple Python script with intentional typos and troubleshoot the errors.
+print(Hello World!)
+# There is a syntax error; missing quotation marks "".
+# Correct code:
+print("Hello World")
+# Next
+PRIN("How are you?")
+# There are two errors in the line above. Firstly, "print" is misspelled. Second, the letters are capital and they need to be lowercase.
