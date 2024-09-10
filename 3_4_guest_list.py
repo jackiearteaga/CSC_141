cfb2aca6ff0045b477b2543db20f2a6d6e5d259a
Jackie Arteaga <guadalupe.arteaga001@albright.edu>
2024-09-10 08:11:21 -0400

    Add files via upload

--- 3_4_guest_list.py
+++ 3_4_guest_list.py
@@ -3,4 +3,4 @@ guests = ["Charles Darwin", "Abraham Lincoln", "Marilyn Monroe"]
 
 # Invite
 for guest in guests:
-    print(f"{guest}, please join me for dinner this evening at 6:30pm. \mLove, \nJackie Arteaga")
+    print(f"{guest}, please join me for dinner this evening at 6:30pm. \nLove, \nJackie Arteaga")
---
+++ 3_5_changing_guest_list.py
@@ -0,0 +1,19 @@
+# Guests
+guests = ["Charles Darwin", "Abraham Lincoln", "Marilyn Monroe"]
+
+# Invite
+for guest in guests:
+    print(f"{guest}, please join me for dinner this evening at 6:30pm. \mLove, \nJackie Arteaga")
+
+# Unable to go
+not_Attending = "Charles Darwin"
+print(f"Sadly, {not_Attending} won't be able to make it. \n")
+
+# Remove and eplace
+guests.remove(not_Attending)
+new_Person = "Princess Diana"
+guests.append(new_Person)
+
+# Newer invites
+for guest in guests:
+    print(f"{guest}, please join me for dinner this evening at 6:30pm. \nLove, \nJackie Arteaga")
